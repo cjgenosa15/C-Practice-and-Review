a98b8f01727df1fe6934fecda3e0a28bcaf07069
cjgenosa15 <cgenosa2@gmail.com>
2026-07-07 11:54:34 +0800

    new function practice

---
+++ 07072026-FUNCTION-PRACTICE.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+void isEven(int num)
+{
+    if(num % 2 == 0)
+    {
+        printf("%d is Even\n", num);
+    }
+    
+    else
+    {
+        printf("%d is Odd\n", num);
+    }
+}
+
+void isPrime(int num)
+{
+    if(num < 2)
+    {
+        printf("%d is Not Prime\n", num);
+        return;
+    }
+
+    else if(num % 2 == 0)
+    {
+        printf("%d is Not Prime\n", num);
+        return;
+    }
+
+    else
+    {
+        for(int i = 2; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                printf("%d is Not Prime\n", num);
+                return;
+            }
+        }
+        printf("%d is  Prime\n", num);
+    }
+}
+
+int power(int base, int pow)
+{
+    int ans = base;
+    
+    for(int i = 1; i < pow; i++)
+    {
+        ans *= base;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    isEven(4);
+    isEven(7);
+    isPrime(7);
+    isPrime(10);
+    printf("2^3 = %d\n", power(2, 3));
+    printf("3^4 = %d\n", power(3, 4));
+    return 0;
+}
