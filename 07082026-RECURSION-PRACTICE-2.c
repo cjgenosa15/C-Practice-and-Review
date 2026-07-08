6d70de4580ea7a6bca84c80d070e86950d2e3ce4
cjgenosa15 <cgenosa2@gmail.com>
2026-07-08 20:46:19 +0800

    pascal recursion problem

---
+++ 07082026-RECURSION-PRACTICE-2.c
@@ -0,0 +1,45 @@
+/* RECURSION PRACTICE - PASCAL'S TRIANGLE
+ * Write a recursive function pascal(row, col) that
+ * calculates each value in Pascal's Triangle.
+ * Ask the user for the number of rows and print
+ * the triangle using nested loops in main.
+ *
+ * Rules:
+ * - Edges are always 1 (when col == 0 OR col == row)
+ * - Every middle value = sum of two values above it
+ *
+ * Expected Output for 5 rows:
+ * 1
+ * 1 1
+ * 1 2 1
+ * 1 3 3 1
+ * 1 4 6 4 1
+ */
+#include <stdio.h>
+
+int pascal(int row, int col)
+{
+    if(col == 0 || col == row)
+    {
+        return 1;
+    }
+
+    return ((pascal(row - 1, col - 1)) + (pascal(row - 1, col)));
+}
+
+int main() {
+    int rows;
+    
+    printf("Enter rows: ");
+    scanf("%d", &rows);
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("%d ", pascal(i, j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
